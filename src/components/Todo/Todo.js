5c50ac06476beea9599e8a778fb89bdbf9020b10
NightThinker <travel_special@hotmail.com>
2019-10-21 13:20:48 +0700

    Avoiding Unnecessary Re-Rendering useMemo

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useRef } from 'react';
+import React, { useState, useEffect, useReducer, useRef, useMemo } from 'react';
 import axios from 'axios';
 
 import List from '../List/List';
@@ -90,7 +90,7 @@ const Todo = () => {
 			<button type='button' onClick={todoAddHandler}>
 				Add
 			</button>
-			<List items={todoList} onClick={todoRemoveHandler} />
+			{useMemo(() => <List items={todoList} onClick={todoRemoveHandler} />, [ todoList ])}
 		</React.Fragment>
 	);
 };
